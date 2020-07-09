c1a2c40bbd636f9f8e58f6311f1ef83977fdc5c7
Raja Roy chowdhury <raja@rajas-mbp.home>
2020-07-09 12:08:59 +0100

    Updating webpage class and test class

--- src/test/java/com/sparta/arc/SGRegForm.java
+++ src/test/java/com/sparta/arc/SGRegForm.java
@@ -1,7 +1,27 @@
 package com.sparta.arc;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class SGRegForm {
+    WebDriver webDriver;
+    By signIn = new By.ByLinkText("sign in");
+
+    public SGRegForm(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public SGRegForm goToHomePage(){
+        webDriver.manage().window().maximize();
+        webDriver.get("http://localhost:9292");
+        return this;
+    }
+
+    public String getURl(){
+        return webDriver.getCurrentUrl();
+    }
+    
+
 }
 
--- src/test/java/com/sparta/arc/SGRegFormTester.java
+++ src/test/java/com/sparta/arc/SGRegFormTester.java
@@ -1,6 +1,23 @@
 package com.sparta.arc;
 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SGRegFormTester {
+
+    WebDriver webDriver = new ChromeDriver();
+    SGRegForm sgRegForm = new SGRegForm(webDriver);
+
+    @Test
+    @DisplayName("Checking website URL is correct")
+    public void siteUrlIsCorrect(){
+        sgRegForm = sgRegForm.goToHomePage();
+        Assertions.assertEquals("http://localhost:9292/",sgRegForm.getURl());
+        webDriver.quit();
+    }
 
 }
